c6033ab424d9aac5d857e618e5eb7cf453911782
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-17 23:36:15 -0300

    feat: :sparkles: error middleware criado

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/errorMiddleware';
 
 const app = express();
+
+app.use(errorHandler);
 
 export default app;
