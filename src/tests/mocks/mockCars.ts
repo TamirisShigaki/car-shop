621437b8853561fe4c71b7d1196d7e05e4f32679
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-18 01:39:12 -0300

    feat: :sparkles: arquivo mock para os test dos arquivos model, service e controller

---
+++ src/tests/mocks/mockCars.ts
@@ -0,0 +1,27 @@
+export const carWithIdMock = {
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    doorsQty: 2,
+    seatsQty: 2,
+    _id: "632242cce100cb6a21015928",
+  }
+  
+  export const createCarMock = {
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    doorsQty: 2,
+    seatsQty: 2,
+  }
+  
+  export const createWithErrorCarMock = {
+    model: "",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    doorsQty: 2,
+    seatsQty: 2,
+  }
