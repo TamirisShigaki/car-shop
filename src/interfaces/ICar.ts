2dccce3887b5d140809874ddcb2ec7f80c232805
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-17 22:11:12 -0300

    refactor: :recycle: add msg de error - ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,8 +2,14 @@ import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
 const CarZodSchema = VehicleZodSchema.extend({
-  doorsQty: z.number().int().gte(2).lte(4),
-  seatsQty: z.number().int().gte(2).lte(7),
+  doorsQty: z.number({
+    required_error: 'DoorsQty is required',
+    invalid_type_error: 'DoorsQty must be a string',
+  }).int().gte(2).lte(4),
+  seatsQty: z.number({
+    required_error: 'SeatsQty is required',
+    invalid_type_error: 'SeatsQty must be a string',
+  }).int().gte(2).lte(7),
 });
 
 type ICar = z.infer<typeof CarZodSchema>;
