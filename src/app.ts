d623953a7d598d98f8835ddf8433a4acadbc1d32
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-18 00:41:20 -0300

    feat: :sparkles: arquivo de rotas Car criado

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/errorMiddleware';
 
+import carRouter from './routes/CarRoutes';
+
 const app = express();
 
+app.use(express.json());
+
+app.use('/cars', carRouter);
 app.use(errorHandler);
 
 export default app;
