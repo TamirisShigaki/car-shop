ffead02148de510e3cd98fda50383b6559e96fdb
Tamiris Shigaki <shigakiendo@gmail.com>
2022-09-18 00:40:36 -0300

    feat: :sparkles: arquivo CarService criado

---
+++ src/services/CarService.ts
@@ -0,0 +1,17 @@
+import { CarZodSchema, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
+
+export default class CarService implements IService<ICar> {
+  constructor(private model: IModel<ICar>) {
+
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const result = CarZodSchema.safeParse(obj);
+    if (!result.success) {
+      throw result.error;
+    }
+    return this.model.create(obj);
+  }
+}
